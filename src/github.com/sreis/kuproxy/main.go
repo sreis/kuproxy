e29a6e24aa0e503be51e9de8b28c6f496ca21645
sreis <smnrsti@gmail.com>
2015-05-26 23:33:01 +0100

    Handle Ctrl-C

--- src/github.com/sreis/kuproxy/main.go
+++ src/github.com/sreis/kuproxy/main.go
@@ -3,23 +3,40 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/sreis/kuproxy/haproxy"
 	"github.com/sreis/kuproxy/keystore"
 )
 
-//
 func main() {
 
 	var master *string = flag.String("master", "http://172.17.8.101:2379", "Etcd master connection url.")
 	flag.Parse()
 
 	if err := haproxy.Start(); err != nil {
-		log.Fatal(err)
+		log.Fatal("Error launching haproxy. '", err, "'.")
 		return
 	}
 
-	keystore.Watch(*master)
+	haproxy.ShowStat()
 
-	haproxy.Stop()
+	// Handle Ctrl-C
+	go func() {
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, os.Interrupt)
+
+		<-sigchan
+
+		if err := haproxy.Stop(); err != nil {
+			log.Fatal("Error stopping haproxy. '", err, "'.")
+		}
+
+		os.Exit(0)
+	}()
+
+	log.Println("Watching and waiting for pods to come online.")
+	log.Println("Press Ctrl-C to exit...")
+	keystore.Watch(*master)
 }
